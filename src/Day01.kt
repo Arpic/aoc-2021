ec82271c34d2bb85070307cbf19fe460bf3cbeab
maxime <maxime@genohm.com>
2021-12-01 23:20:45 +0100

    Day 1 Part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,9 @@
+import java.util.stream.Collectors
+import kotlin.streams.toList
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.zipWithNext{a,b -> if (a.toInt() < b.toInt()) 1 else 0 }.stream().collect(Collectors.summingInt { d -> d.toInt() })
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +12,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
