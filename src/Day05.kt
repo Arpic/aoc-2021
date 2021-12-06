8bed2dffae164b6feb947baa48acd2745cd2e236
Namlin <arpic@users.noreply.github.com>
2021-12-06 11:24:41 +0100

    Day 5 Part 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -5,7 +5,7 @@ fun main() {
 
     data class Coordinate(val x: Int, val y: Int)
 
-    class Line(start: Coordinate, end: Coordinate) {
+    class Line(start: Coordinate, end: Coordinate, withDiagonals: Boolean) {
 
         val vents: List<Coordinate>
 
@@ -13,8 +13,23 @@ fun main() {
             when {
                 start.x == end.x -> List(abs(start.y - end.y) + 1) { i -> Coordinate(start.x, min(start.y, end.y) + i) }
                 start.y == end.y -> List(abs(start.x - end.x) + 1) { i -> Coordinate(min(start.x, end.x) + i, start.y) }
+                withDiagonals && abs(start.x-end.x) == abs(start.y - end.y) -> buildDiagonal(start, end)
                 else -> emptyList()
             }.also { vents = it }
+        }
+
+        private fun buildDiagonal(start: Coordinate, end: Coordinate): List<Coordinate> {
+            val tempList = mutableListOf<Coordinate>()
+            val xIncrement = if (start.x < end.x) 1 else -1
+            val yIncrement = if (start.y < end.y) 1 else -1
+            var x = start.x
+            var y = start.y
+            while (x != end.x + xIncrement && y != end.y + yIncrement) {
+                tempList.add(Coordinate(x, y))
+                x += xIncrement
+                y += yIncrement
+            }
+            return tempList.toList()
         }
 
         override fun toString(): String {
@@ -22,18 +37,23 @@ fun main() {
         }
     }
 
+    fun extractCoordinates(coordinateString: String): List<Coordinate> {
+        val coordinates = "(\\d+,\\d+) -> (\\d+,\\d+)".toRegex()
+            .find(coordinateString)?.destructured?.toList()
+            ?.map { it.split(",") }
+            ?.map { Coordinate(it[0].toInt(), it[1].toInt()) }
+            ?.toList()
+        assert(coordinates!!.size == 2)
+        return coordinates
+    }
+
     fun part1(input: List<String>): Int {
         val lines = mutableListOf<Line>()
         val singleHit = mutableSetOf<Coordinate>()
         val doubleHit = mutableSetOf<Coordinate>()
         input.forEach { coordinateString ->
-            val coordinates = "(\\d+,\\d+) -> (\\d+,\\d+)".toRegex()
-                .find(coordinateString)?.destructured?.toList()
-                ?.map { it.split(",") }
-                ?.map { Coordinate(it[0].toInt(), it[1].toInt()) }
-                ?.toList()
-            assert(coordinates!!.size == 2)
-            lines.add(Line(coordinates[0], coordinates[1]))
+            val coordinates = extractCoordinates(coordinateString)
+            lines.add(Line(coordinates[0], coordinates[1], false))
         }
 
         lines
@@ -43,18 +63,29 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val lines = mutableListOf<Line>()
+        val singleHit = mutableSetOf<Coordinate>()
+        val doubleHit = mutableSetOf<Coordinate>()
+        input.forEach { coordinateString ->
+            val coordinates = extractCoordinates(coordinateString)
+            lines.add(Line(coordinates[0], coordinates[1], true))
+        }
+
+        lines
+            .flatMap { it.vents }
+            .filterNotTo(doubleHit) { singleHit.add(it) }
+        return doubleHit.size
     }
 
     val testInput = readInput("Day05_test")
     val part1 = part1(testInput)
     println(part1)
-//    val part2 = part2(testInput)
-//    println(part2)
+    val part2 = part2(testInput)
+    println(part2)
     check(part1 == 5)
-//    check(part2 == 230)
+    check(part2 == 12)
 
     val input = readInput("Day05")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
